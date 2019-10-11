90ce76fda1d73e7236e3c6120a3ab69fd04d966b
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-10-11 07:54:19 +0300

    Add fields. Create constructor.

--- GeekBrains_OOP/src/Employee.java
+++ GeekBrains_OOP/src/Employee.java
@@ -1,2 +1,21 @@
 public class Employee {
+    String FIO;
+    String position;
+    String email;
+    long tel;
+    int salary;
+    int age;
+
+    public static void main(String[] args) {
+
+    }
+
+    public Employee(){
+        FIO = "Иванов Иван Иванович";
+        position = "инженер";
+        email = "IvanovII@mail.ru";
+        tel = 89995552288L;
+        salary = 40000;
+        age = 25;
+    }
 }
