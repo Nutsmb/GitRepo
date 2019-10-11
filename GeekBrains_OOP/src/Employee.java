0af42bca074876d1cab569c07ca8ef7f2bfac599
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-10-11 07:57:26 +0300

    Add getPersonInfo(). Add getters.

--- GeekBrains_OOP/src/Employee.java
+++ GeekBrains_OOP/src/Employee.java
@@ -1,4 +1,6 @@
 public class Employee {
+    public Object getPersonInfo;
+    
     String FIO;
     String position;
     String email;
@@ -18,4 +20,36 @@ public class Employee {
         salary = 40000;
         age = 25;
     }
+
+    public static void getPersonInfo(Employee person) {
+        System.out.println("Сотрудник "+ person.getFIO() + " (возраст " + person.getAge() + ")" + " на позиции " + person.getPosition() + " с зарплатой " + person.getSalary());
+        System.out.println("E-mail: " + person.getEmail());
+        System.out.println("Tel.: " + person.getTel());
+    }
+
+    public String getFIO() {
+        return FIO;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getTel() {
+        return tel;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
 }
