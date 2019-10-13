3c1dcfed1f320681547472107c19734f5c41329b
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-10-13 11:54:18 +0300

    Add setters. Add constructor code. Fill persArray. Chose persons in persArray.

--- GeekBrains_OOP/src/Employee.java
+++ GeekBrains_OOP/src/Employee.java
@@ -9,19 +9,16 @@ public class Employee {
     int age;
 
     public Employee(String FIO, String position, String email, long tel, int salary, int age) {
+        setFIO(FIO);
+        setPosition(position);
+        setEmail(email);
+        setTel(tel);
+        setSalary(salary);
+        setAge(age);
     }
 
     public static void main(String[] args) {
 
-    }
-
-    public Employee(){
-        FIO = "Иванов Иван Иванович";
-        position = "инженер";
-        email = "IvanovII@mail.ru";
-        tel = 89995552288L;
-        salary = 40000;
-        age = 25;
     }
 
     public static void getPersonInfo(Employee person) {
@@ -54,5 +51,27 @@ public class Employee {
         return age;
     }
 
+    public void setFIO(String _FIO) {
+        FIO = _FIO;
+    }
 
+    public void setPosition(String _position) {
+        position = _position;
+    }
+
+    public void setEmail(String _email) {
+        email = _email;
+    }
+
+    public void setTel(long _tel) {
+        tel = _tel;
+    }
+
+    public void setSalary(int _salary) {
+        salary = _salary;
+    }
+
+    public void setAge(int _age) {
+        age = _age;
+    }
 }
--- GeekBrains_OOP/src/New_Employees.java
+++ GeekBrains_OOP/src/New_Employees.java
@@ -3,9 +3,17 @@ public class New_Employees {
     public static void main(String[] args) {
         Employee[] persArray = new Employee[5];
         persArray[0] = new Employee("Ivanov Ivan", "Engineer", " ivivan@mailbox.com ", 892312312, 30000, 30);
-        persArray[0] = new Employee("Ivanov Ivan", "Engineer", " ivivan@mailbox.com ", 892312312, 30000, 30);
-        persArray[0] = new Employee("Ivanov Ivan", "Engineer", " ivivan@mailbox.com ", 892312312, 30000, 30);
-        persArray[0] = new Employee("Ivanov Ivan", "Engineer", " ivivan@mailbox.com ", 892312312, 30000, 30);
-        persArray[0] = new Employee("Ivanov Ivan", "Engineer", " ivivan@mailbox.com ", 892312312, 30000, 30);
+        persArray[1] = new Employee("Sergeeva Anna", "Team Leader", " seranna@mailbox.com ", 892376439, 50000, 42);
+        persArray[2] = new Employee("Korshun Nikola", "Programmer", " kornikola@mailbox.com ", 898923571, 40000, 21);
+        persArray[3] = new Employee("Mironov Andrey", "Director", " mirandrey@mailbox.com ", 893927164, 70000, 56);
+        persArray[4] = new Employee("Jekolov Alex", "Tech.leader", " jekalex@mailbox.com ", 896630261, 60000, 39);
+
+
+        for(int i = 0; i < persArray.length; i++){
+            if(persArray[i].getAge() > 40){
+                persArray[i].getPersonInfo(persArray[i]);
+                System.out.println();
+            }
+        }
     }
 }
