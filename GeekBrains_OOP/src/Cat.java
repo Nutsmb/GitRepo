b915392827c15f8e2ec2c96bced9f7e3d8c57b4c
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-10-18 20:39:32 +0300

    Add name, appetite, hungry for Cats. Add method eat().

--- GeekBrains_OOP/src/Cat.java
+++ GeekBrains_OOP/src/Cat.java
@@ -1,17 +1,23 @@
 public class Cat extends Animal{
+    String name;
     float MAX_JUMP_HEIGHT;
     float MAX_RUN_LENGTH;
+    int APPETITE;
+    boolean hungry;
 
     public static void main(String[] args) {
-        Animal cat1 = new Cat(2, 200);
+        Animal cat1 = new Cat("Matroskin",2, 200, 10);
         System.out.println(cat1.jump(1));
         System.out.println(cat1.run(200));
         System.out.println(cat1.swim(2));
     }
 
-    public Cat(float _jump, float _run){
+    public Cat(String _name, float _jump, float _run, int _appetite){
+        name = _name;
         MAX_JUMP_HEIGHT = _jump;
         MAX_RUN_LENGTH = _run;
+        APPETITE = _appetite;
+        hungry = true;
     }
 
     @Override
@@ -35,5 +41,17 @@ public class Cat extends Animal{
             result = true;
         }
         return result;
+    }
+
+    public void eat(int cupOfMeal){
+        if(APPETITE > cupOfMeal){
+            hungry = true;
+            System.out.println(this.name+"'у мало еды в этой миске");
+        }
+        else {
+            cupOfMeal = cupOfMeal - APPETITE;
+            hungry = false;
+        }
+
     }
 }
